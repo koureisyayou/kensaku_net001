998a748e7c3f344dfc0d710b6a3946ea3834a2c9
koureisyayou <kaiitukiya@gmail.com>
2026-08-30 03:24:53 +0900

    Update update_financials.py

--- update_financials.py
+++ update_financials.py
@@ -136,7 +136,7 @@ def request_with_retry(url, params=None, headers=None, retries=3, backoff_factor
 
 
 def get_submitted_documents(date_str):
-    url = f"https://api.edinet-fsa.go.jp/api/v2/documents/{doc_id}"
+    url = "https://api.edinet-fsa.go.jp/api/v2/documents.json"
 
     params = {
         "date": date_str,
